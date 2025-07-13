717d83a53f6cdf053c432e8bd68cb0c4fa344fa8
sahildahiya1302 <146929731+sahildahiya1302@users.noreply.github.com>
2025-07-13 17:29:36 +0530

    Revert "Add hero banner blocks"

--- admin/components/footer.php
+++
@@ -1,5 +0,0 @@
-</div>
-</div>
-<script src="/admin/assets/admin.js"></script>
-</body>
-</html>
--- themes/default/assets/custom.js
+++
@@ -1,9 +0,0 @@
-function custom(){console.log('custom script');}
-
-function trackEvent(type, data={}) {
-  fetch('/api/events.php', {
-    method: 'POST',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({type: type, data: data})
-  }).catch(() => {});
-}
